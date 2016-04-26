61672471c25346f3c50ac8879cea5255cd4a30ef
VolleyTKE <chris.a.carr@gmail.com>
2016-04-25 19:55:18 -0500

    write exercise 3 with floating point numbers 2.0 not 2

--- LPTHW/ex3.py
+++ LPTHW/ex3.py
@@ -1,23 +1,25 @@
+
+
 #prints a string literal
 print "I will now count my chickens:"
 
 #Prints hens then does division followed by addition
-print "Hens", 25 + 30 / 6
+print "Hens", 25.00 + 30.0 / 6.0
 #prints roosters then does *, % finally -
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25 * 3 % 4
 
 #Prints a string
 print "Now I will count the eggs:"
 
 #does math in order of operations
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 #compares to numbers
-print 3 + 2 < 5 - 7
+print 3.0 + 2 < 5.0 - 7
 
 #prints a string then displays math
-print "What is 3 + 2?", 3 + 2
-print "What is 5 -7?" , 5 - 7
+print "What is 3 + 2?", 3.0 + 2
+print "What is 5 -7?" , 5.0 - 7
 
 #prints a string
 print "Oh, that's why it's False."
@@ -25,6 +27,6 @@ print "Oh, that's why it's False."
 print "How about some more."
 
 #compares to numbers for equality returns a boolean value
-print "is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+print "is it greater?", 5.0 > -2
+print "Is it greater or equal?", 5.0 >= -2
+print "Is it less or equal?", 5.0 <= -2
