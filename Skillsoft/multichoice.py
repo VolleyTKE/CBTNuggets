18c160517b7f2012aff40ffbbe630fab8eda1571
VolleyTKE <chris.a.carr@gmail.com>
2016-04-26 08:04:40 -0500

    add control flow to take user input and return a result

--- Skillsoft/multichoice.py
+++ Skillsoft/multichoice.py
@@ -1,14 +1,25 @@
 def main():
 
-	choices = dict(
-	alex = "A",
-	jane ="B",
-	tim = "C",
-	john = "A",
-	kim="D"
-	)
+	#choices = dict(
+	#alex = "A",
+	#jane ="B",
+	#tim = "C",
+	#john = "A",
+	#kim="D"
+	#)
 	
+	usersel = raw_input("Please pick a letter A-D: ")
+	if usersel == "A":
+		print("alex, john")
+	elif usersel == "B":
+		print("jane")
+	elif usersel == "C":
+		print("tim")
+	elif usersel == "D":
+		print("kim")
+	else:
+		print("nothing selected")
 	
-	print(choices["alex"])
+	#print(choices["alex"])
 	
 main()
