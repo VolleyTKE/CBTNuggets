da48c865b93e6c063acaa2850b395da05800694b
VolleyTKE <chris.a.carr@gmail.com>
2016-04-29 21:48:34 -0500

    update ex5 to include study drills

--- LPTHW/ex5.py
+++ LPTHW/ex5.py
@@ -1,17 +1,23 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 #not a lie
-my_height = 74 #inches
-my_weight = 180 #lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 #not a lie
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+#conversions for the rest of the world
+inchesToCm = height * 2.54
+lbsToKg = weight / 2.2046
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
-print "His teeth are usualy %s depending on the coffee." % my_teeth
+print "His teeth are usually %s depending on the coffee." % teeth
+print "He's %d centimeters tall and %d kilograms." % (inchesToCm, lbsToKg)
+
 
 #this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
 
