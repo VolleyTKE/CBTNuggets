91a7800fd3ae6979a0d987b7cca55690b1fd0dd0
VolleyTKE <chris.a.carr@gmail.com>
2016-05-05 08:26:15 -0500

    now with comments

--- LPTHW/ex8.py
+++ LPTHW/ex8.py
@@ -1,3 +1,5 @@
+# spacing of  % effects strings at print
+#escape character \t appears only when printing formatter(formatter, ...)
 formatter = "%r 	%r		 %r			 %r"
 
 print formatter % (1, 2, 3, 4)
