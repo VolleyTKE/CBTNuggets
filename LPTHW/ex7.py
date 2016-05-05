154eabf311a1830e04bc53f7273b53356b7e7fb2
VolleyTKE <chris.a.carr@gmail.com>
2016-05-05 07:46:59 -0500

    add comments

--- LPTHW/ex7.py
+++ LPTHW/ex7.py
@@ -1,8 +1,12 @@
+#print a string
 print "Mary had a little lamb."
+#print a formatted string
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
+#print a string 10 times
 print "." * 10 # what'd that do?
 
+# initialize and assign string variables
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,6 +21,6 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
+print end1 + end2 + end3 + end4 + end5 + end6
 print end7 + end8 + end9 + end10 + end11 + end12
 
