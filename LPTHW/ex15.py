3fa5134c9d9f165d84125ae43f5941214865c92e
VolleyTKE <chris.a.carr@gmail.com>
2016-05-16 21:05:51 -0500

    add comments to explain each line

--- LPTHW/ex15.py
+++ LPTHW/ex15.py
@@ -1,16 +1,25 @@
+#import statement for running scripts with arguments.
 from sys import argv
 
+#the two var names for the arguments the script is expecting
 script, filename = argv
 
+#pass the filename in as an argument to make the txt available to read
 txt = open(filename)
 
+#print the words below and the name of the file.
 print "Here's your file %r:" % filename
+#print runs a read method on the previously opened object and prints the contents.
 print txt.read()
 
+#prints the words below
 print "Type the filename again:"
+#create/initialize a var based on user input
 file_again = raw_input("> ")
 
+#create/initialize a var to make the txt available to read
 txt_again = open(file_again)
 
+#print runs a read method on the opened file object and prints the contents
 print txt_again.read()
 
