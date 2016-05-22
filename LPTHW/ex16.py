b727287fdae724e32a69e964bbdc0c2a9958ffb2
VolleyTKE <chris.a.carr@gmail.com>
2016-05-22 00:12:47 -0500

    make exercise 16 with study drills.

--- LPTHW/ex16.py
+++ LPTHW/ex16.py
@@ -12,7 +12,6 @@ print "Opening the file..."
 target = open(filename, 'w')
 
 print "Truncating the file.  Goodbye!"
-target.truncate()
 
 print "Now I'm going to ask you for three lines."
 
@@ -22,12 +21,8 @@ line3 = raw_input("line3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("""%s\n%s\n%s\n""" % (line1, line2, line3))
+
 
 print "And finally, we close it."
 target.close()
