e2d72b3542a6c30808a61b09fbce6759f5214e36
VolleyTKE <chris.a.carr@gmail.com>
2016-11-04 14:22:45 -0500

    exercise 3 and 3a with study drills.

--- lpthw2/ex03.py
+++ lpthw2/ex03.py
@@ -21,3 +21,28 @@ print "How about some more."
 print "Is it greater?", 5 > -2
 print "Is it greater?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
+
+#Study Drill
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30.0 / 6.0
+print "Roosters", 100 - 25 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2", 3 + 2
+print "What is 5 - 7", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ lpthw2/ex03a.py
@@ -0,0 +1,18 @@
+#Study Drill
+print "\nIf the temperature is 0 F, "
+print "in C it is:"
+print (0 - 32) * 5/9
+print "Kelvin is:"
+print (0 - 32) * 5/9 + 273.15
+
+print "\nIf the temperature is 70 F, "
+print "in C it is:"
+print (70 - 32) * 5/9
+print " Kelvin is:"
+print (70 - 32) * 5/9 + 273.15
+
+print "\nIf the temperature is 100 F, "
+print "in C it is:"
+print (100 - 32) * 5/9
+print " Kelvin is:"
+print (100 - 32) * 5/9 + 273.15
