c4b4565adb589fb13a0c8a56f033966dde845529
VolleyTKE <chris.a.carr@gmail.com>
2016-11-13 20:11:34 -0600

    now with Study drills

--- lpthw2/ex05.py
+++ lpthw2/ex05.py
@@ -15,3 +15,23 @@ print "His teeth are usually %s depending on the coffee." % my_teeth
 
 #this line is trick, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+
+#Study Drill
+name = 'Chris A. Carr'
+age = 28 # not a lie
+height = 74 # Inches
+weight = 230 #lbs
+eyes = 'Hazel'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+#this line is trick, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
