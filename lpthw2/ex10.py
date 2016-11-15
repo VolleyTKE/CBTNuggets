9791095ca354a10a0d55e3885e5cab97c23a907c
VolleyTKE <chris.a.carr@gmail.com>
2016-11-15 15:49:26 -0600

    the loop I did was infinite.

--- lpthw2/ex10.py
+++ lpthw2/ex10.py
@@ -13,3 +13,21 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+
+#study drills
+
+while True:
+	for i in ["/","-","|","\\","|"]:
+		print "%s\r" % i,
+
+
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+
