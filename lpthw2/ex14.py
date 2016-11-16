3507bb2ca4598b6a0ca62bea37f28f334e6566ac
VolleyTKE <chris.a.carr@gmail.com>
2016-11-16 08:34:16 -0600

    completed ex14.py script

--- lpthw2/ex14.py
+++ lpthw2/ex14.py
@@ -13,3 +13,11 @@ lives = raw_input(prompt)
 
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
+
+print """
+Alright, so you said %r about liking me.
+You live in %r. Not sure where that is.
+And you have a %r computer. Nice.
+""" % (likes, lives, computer)
+
+
