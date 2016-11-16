100b097c728d99ed2f427c1993a7075411aceb49
VolleyTKE <chris.a.carr@gmail.com>
2016-11-16 09:07:04 -0600

    exercise 14 with study drills

--- lpthw2/ex14.py
+++ lpthw2/ex14.py
@@ -1,12 +1,12 @@
 from sys import argv
 
-script, user_name = argv
-prompt = '> '
+script, user_name, height = argv
+prompt = '%s. Please answer: ' % user_name
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
-likes = raw_input(prompt)
+likes = raw_input(prompt) 
 
 print "Where do you live %s?" % user_name
 lives = raw_input(prompt)
@@ -18,6 +18,7 @@ print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
 And you have a %r computer. Nice.
-""" % (likes, lives, computer)
+I already know you're %s inches tall
+""" % (likes, lives, computer, height)
 
 
