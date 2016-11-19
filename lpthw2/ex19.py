503756d281f012403671600bff2d356728209aae
VolleyTKE <chris.a.carr@gmail.com>
2016-11-18 19:47:45 -0600

    ex19 with study drills
    I combine the question and response portion of ex14 with
    the def portion of 19. I will be committing this and tagging it. v0.1
    My first script.

--- lpthw2/ex19.py
+++ lpthw2/ex19.py
@@ -1,3 +1,7 @@
+from sys import argv
+
+script = argv
+
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
 	print "you have %d cheeses!" % cheese_count
 	print "You have %d boxes of crackers!" % boxes_of_crackers
@@ -21,3 +25,28 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+user_name = raw_input("Please provide your name: ")
+
+
+def user_questions(user_name):
+	prompt = '%s. Please answer: ' % user_name
+
+	print "Hi %s, I'm the %s script." % (user_name, script)
+	print "I'd like to ask you a few questions."
+	print "Do you like me %s?" % user_name
+	likes = raw_input(prompt)
+
+	print "Where do you live %s?" % user_name
+	lives = raw_input(prompt)
+
+	print "What kind of computer do you have?"
+	computer = raw_input(prompt)
+
+	print """
+	Alright, so you said %r about liking me.
+	You live in %r. Not sure where that is.
+	And you have a %r computer. Nice.
+	""" % (likes, lives, computer)
+
+user_questions(user_name)
