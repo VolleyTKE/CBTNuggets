46ae7fa0ac145f7e634ad63f55ad5a93ff627766
VolleyTKE <chris.a.carr@gmail.com>
2016-11-23 10:03:48 -0600

    Ex33 with study drills, but not for loop

--- lpthw2/ex33.py
+++ lpthw2/ex33.py
@@ -1,20 +1,22 @@
 numbers = []
 
-def loop(loopLength):
+def loop(loopLength, inc):
     i = 0
-
     while i < loopLength:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + inc
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
 #don't forget that raw_input is a str so convert to int
 length = int(raw_input("Please enter a number... \n>"))
 
-loop(length)
+increment = int(raw_input("Enter increment size...\n>"))
+
+
+loop(length, increment)
 
 print "The numbers: "
 
