849de5400d63ac9fffab1eab32977c23cf271bd5
VolleyTKE <chris.a.carr@gmail.com>
2016-11-23 04:31:50 -0600

    Turn while into a function

--- lpthw2/ex33.py
+++ lpthw2/ex33.py
@@ -1,13 +1,20 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def loop(loopLength):
+    i = 0
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    while i < loopLength:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+#don't forget that raw_input is a str so convert to int
+length = int(raw_input("Please enter a number... \n>"))
+
+loop(length)
 
 print "The numbers: "
 
