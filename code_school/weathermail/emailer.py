ae752f074125a464b0e3388b45930baa30811e49
VolleyTKE <chris.a.carr@gmail.com>
2017-11-05 21:23:00 -0600

    Can print email dict and schedule file

--- code_school/weathermail/emailer.py
+++ code_school/weathermail/emailer.py
@@ -5,9 +5,17 @@ email_file = open('emails2.txt', 'r')
 try:
     for line in email_file:
         (email, name) = line.split(',')
-        emails[email] = name
+        emails[email] = name.strip()
 
 except FileNotFoundError as err:
     print(err)
 
 print(emails)
+
+try:
+    schedule_file = open('schedule.txt', 'r')
+    schedule = schedule_file.read()
+except FileNotFoundError as err:
+    print(err)
+    
+print(schedule)
