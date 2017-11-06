a7ad402eb8c6720352c274d0d4e941d20794dbbc
VolleyTKE <chris.a.carr@gmail.com>
2017-11-05 21:29:06 -0600

    Add functions including main()

--- code_school/weathermail/emailer.py
+++ code_school/weathermail/emailer.py
@@ -1,21 +1,32 @@
-emails = {}
+def get_emails():
+    emails = {}
 
-email_file = open('emails2.txt', 'r')
+    email_file = open('emails2.txt', 'r')
 
-try:
-    for line in email_file:
-        (email, name) = line.split(',')
-        emails[email] = name.strip()
+    try:
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
 
-except FileNotFoundError as err:
-    print(err)
+    except FileNotFoundError as err:
+        print(err)
 
-print(emails)
+    return emails
 
-try:
-    schedule_file = open('schedule.txt', 'r')
-    schedule = schedule_file.read()
-except FileNotFoundError as err:
-    print(err)
-    
-print(schedule)
+def get_schedule():
+    try:
+        schedule_file = open('schedule.txt', 'r')
+        schedule = schedule_file.read()
+    except FileNotFoundError as err:
+        print(err)
+
+    return schedule
+
+def main():
+    emails = get_emails()
+    print(emails)
+
+    schedule = get_schedule()
+    print(schedule)
+
+main()
